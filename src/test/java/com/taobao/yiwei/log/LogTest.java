dadf2799971517197003226cca31b40d1579bc21
skyxzg <skyxzg@aliyun.com>
2015-07-28 17:31:16 +0800

    add log test

---
+++ src/test/java/com/taobao/yiwei/log/LogTest.java
@@ -0,0 +1,36 @@
+package com.taobao.yiwei.log;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LogTest extends BaseTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		assertTrue(true);
+		log.info("now {}", "print log...");
+	}
+
+}
