47ddd6c846d35363bb056790837fc49f1c2bafc9
zhigang.xzg <zhigang.xzg@alibaba-inc.com>
2019-06-11 11:48:30 +0800

    semaphore

---
+++ src/main/java/com/taobao/yiwei/thread/AbnormalSemaphoreSample.java
@@ -0,0 +1,49 @@
+package com.taobao.yiwei.thread;
+
+import java.util.concurrent.Semaphore;
+
+
+public class AbnormalSemaphoreSample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(0);
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new MyWorker(semaphore));
+            t.start();
+        }
+        System.out.println("Action...GO!");
+        semaphore.release(5);
+        System.out.println("Wait for permits off");
+        while (semaphore.availablePermits() != 0) {
+            Thread.sleep(100L);
+        }
+        System.out.println("Action...GO again!");
+        semaphore.release(5);
+    }
+}
+
+class MyWorker implements Runnable {
+    private String name;
+    private Semaphore semaphore;
+
+    public MyWorker(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            semaphore.acquire();
+            log("Permit acquired!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void log(String msg) {
+        if (name == null) {
+            name = Thread.currentThread().getName();
+        }
+        System.out.println(name + " " + msg);
+    }
+}
---
+++ src/main/java/com/taobao/yiwei/thread/UsualSemaphoreSample.java
@@ -0,0 +1,48 @@
+package com.taobao.yiwei.thread;
+
+import java.util.concurrent.Semaphore;
+
+
+public class UsualSemaphoreSample {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Action...GO!");
+        Semaphore semaphore = new Semaphore(5);
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new SemaphoreWorker(semaphore));
+            t.start();
+        }
+    }
+}
+
+class SemaphoreWorker implements Runnable {
+    private String name;
+    private Semaphore semaphore;
+
+    public SemaphoreWorker(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        try {
+            log("is waiting for a permit!");
+            semaphore.acquire(); // 如果获取不到信号量，阻塞在这里
+            log("acquired a permit!");
+            // do something
+            Thread.sleep(5000);
+            log("executed!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log("released a permit!");
+            semaphore.release();
+        }
+    }
+
+    private void log(String msg) {
+        if (name == null) {
+            name = Thread.currentThread().getName();
+        }
+        System.out.println(name + " " + msg);
+    }
+}
