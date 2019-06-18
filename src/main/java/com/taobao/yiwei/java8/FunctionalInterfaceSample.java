3bf757ff753984f4745a4a5ebd81695c2253b351
zhigang.xzg <zhigang.xzg@alibaba-inc.com>
2019-06-18 16:58:16 +0800

    MethodReferencesSample

--- src/main/java/com/taobao/yiwei/java8/FunctionalInterfaceSample.java
+++ src/main/java/com/taobao/yiwei/java8/FunctionalInterfaceSample.java
@@ -8,8 +8,18 @@ import java.util.function.Predicate;
 
 /**
  * 函数式接口（Functional Interface）就是一个有且仅有一个抽象方法，但是可以有多个非抽象方法的接口。
+ * 一开始也被叫做SAM类型的接口（Single Abstract Method）
  * 函数式接口可以被隐式转换为 lambda 表达式。
  * 使用 @FunctionalInterface 注释可以确保，如果在未来更改该接口时意外违反抽象方法数量规则，您会获得错误消息。
+ *
+ * 更多：
+ * 为什么会单单从接口中定义出此类接口呢？
+ * 原因是在Java Lambda的实现中， 开发组不想再为Lambda表达式单独定义一种特殊的Structural函数类型，称之为箭头类型（arrow type），
+ * 依然想采用Java既有的类型系统(class, interface, method等)， 原因是增加一个结构化的函数类型会增加函数类型的复杂性，破坏既有的Java类型，
+ * 并对成千上万的Java类库造成严重的影响。 权衡利弊， 因此最终还是利用SAM 接口作为 Lambda表达式的目标类型。
+ * JDK中已有的一些接口本身就是函数式接口，如Runnable。 JDK 8中又增加了java.util.function包， 提供了常用的函数式接口。
+ * 函数式接口代表的一种契约， 一种对某个特定函数类型的契约。 在它出现的地方，实际期望一个符合契约要求的函数。 Lambda表达式不能脱离上下文而存在，
+ * 它必须要有一个明确的目标类型，而这个目标类型就是某个函数式接口。
  */
 @FunctionalInterface
 interface GreetingService {
--- src/main/java/com/taobao/yiwei/java8/MethodReferencesSample.java
+++ src/main/java/com/taobao/yiwei/java8/MethodReferencesSample.java
@@ -13,6 +13,25 @@ public class MethodReferencesSample {
         names.add("Baidu");
         names.add("Sina");
 
+
+        // 1. 引用静态方法
+        names.sort(String::compareTo);
+
+        // 2. 引用某个特定对象的实例方法
         names.forEach(System.out::println);
+
+        // 3. 引用某个类的实例方法
+        // lambda
+//        names.stream().map(name -> name.length()).forEach(System.out::println);
+        // method reference
+        names.stream().map(String::length).forEach(System.out::println);
+
+        // 4. 引用类的构造函数
+        // lambda
+//        names.stream().map(name -> {
+//            return new StringBuilder(name);
+//        }).forEach(System.out::println);
+        // method reference
+        names.stream().map(StringBuilder::new).forEach(System.out::println);
     }
 }
