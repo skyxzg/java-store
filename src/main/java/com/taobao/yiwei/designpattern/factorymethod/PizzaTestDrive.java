b108eb1edb5b210169c159c72c7ebc9683bc7c0d
skyxzg <skyxzg@aliyun.com>
2019-06-21 00:33:40 +0800

    comments

--- src/main/java/com/taobao/yiwei/designpattern/factorymethod/PizzaTestDrive.java
+++ src/main/java/com/taobao/yiwei/designpattern/factorymethod/PizzaTestDrive.java
@@ -1,5 +1,9 @@
 package com.taobao.yiwei.designpattern.factorymethod;
 
+/**
+ * 工厂方法模式
+ * 定义了一个创建对象的接口，但由子类决定要实例化的类是哪一个。工厂方法让类把实例化推迟到子类。
+ */
 public class PizzaTestDrive {
     public static void main(String[] args) {
         PizzaStore nyStore = new NYPizzaStore();
