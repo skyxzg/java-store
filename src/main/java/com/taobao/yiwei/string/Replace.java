59545e4c9fd194d1fc27d175e1b0f3dfdea4f7b5
zhigang.xzg <zhigang.xzg@alibaba-inc.com>
2020-02-21 22:34:50 +0800

    string replace

--- src/main/java/com/taobao/yiwei/string/Replace.java
+++ src/main/java/com/taobao/yiwei/string/Replace.java
@@ -4,8 +4,9 @@ public class Replace {
 
 	public static void main(String[] args){
 		String testStr = "{binary,auth},{binary,noauth}";
-		System.out.println(testStr);
-		testStr = testStr.replace("{", "");
-		System.out.println(testStr);
+		System.out.println("testStr:" + testStr);
+		String newStr = testStr.replace("{", "");
+		System.out.println("testStr:" + testStr);
+		System.out.println("newStr:" + newStr);
 	}
 }
